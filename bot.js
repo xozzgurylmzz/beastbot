c9445836899019ca1e2855d2d846b1eef88fc1db
xozzgurylmzz <82913006+xozzgurylmzz@users.noreply.github.com>
2021-04-21 01:24:12 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,138 @@
+const fs=require('fs');
+const Discord=require("discord.js");
+const client=new Discord.Client();
+const db = require('quick.db')
+const moment = require("moment");
+const ayarlar=require("./ayarlar.json");
+const express = require('express');
+/////
+const app = express()
+app.get('/', (req, res) => res.send("Bot Aktif"))
+app.listen(process.env.PORT, () => console.log('Port ayarlandı: ' + process.env.PORT))
+//////////////////
+
+
+
+client.on("message", message => {
+  let client = message.client;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.yetkiler(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
+})
+
+const Gamedig = require('gamedig');
+let ip = "208.103.169.219"
+Gamedig.query({
+    type: 'garrysmod',
+    host: ip
+}).then((state) => {
+    client.user.setActivity(`${state.raw.numplayers} / ${state.maxplayers} || ${state.map}`, `${state.raw.numplayers} / ${state.maxplayers}`, {type: 'PLAYING'}) 
+}).catch((error) => {
+    return
+})
+
+const log = message => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
+};
+
+client.on('ready', () => {
+  client.channels.cache.get('832921833983705092').join();
+})
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} adet komut yüklemeye hazırlanılıyor.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut ismi: ${props.help.name.toUpperCase()}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+  
+client.yetkiler = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = -ayarlar.varsayilanperm  ;
+  if(message.member.hasPermission("MANAGE_MESSAGES")) permlvl = 1;
+  if(message.member.hasPermission("KICK_MEMBERS")) permlvl = 2;
+  if(message.member.hasPermission("BAN_MEMBERS")) permlvl = 3;
+  if(message.member.hasPermission("MANAGE_GUILD")) permlvl = 4;
+  if(message.member.hasPermission("ADMINISTRATOR")) permlvl = 5;
+  if(message.author.id === message.guild.ownerID) permlvl = 6;
+  if(message.author.id === ayarlar.sahip) permlvl = 7;
+  return permlvl;
+};
+
+
+
+client.login(ayarlar.token)
